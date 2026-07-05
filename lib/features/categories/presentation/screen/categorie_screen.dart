cb4d074a40965ba58e9b74ea46b52b6109585400
Mahmoud Hamam <mahmoudhamam093@gmail.com>
2026-07-05 03:18:08 +0300

    Refactor: categoreis ui

--- lib/features/categories/presentation/screen/categorie_screen.dart
+++ lib/features/categories/presentation/screen/categorie_screen.dart
@@ -3,7 +3,6 @@ import 'package:nti_ecommerce_team4/core/theme/app_theme.dart';
 import 'package:nti_ecommerce_team4/features/categories/presentation/widget/collections_gridview.dart';
 import 'package:nti_ecommerce_team4/features/categories/presentation/widget/textfield.dart';
 import 'package:nti_ecommerce_team4/features/home/presentation/widgets/drawer.dart';
-import 'package:nti_ecommerce_team4/features/profile/presentation/widgets/dark_mode_toggle.dart';
 
 class CategorieScreen extends StatelessWidget {
   const CategorieScreen({super.key});
--- lib/features/products/presentation/screens/admin_product_management_screen.dart
+++ lib/features/products/presentation/screens/admin_product_management_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nti_ecommerce_team4/core/theme/app_theme.dart';
 import 'package:nti_ecommerce_team4/features/auth/presentation/widgets/add_product_list_view.dart';
-import 'package:nti_ecommerce_team4/features/profile/presentation/widgets/dark_mode_toggle.dart';
 
 class AdminProductManagement extends StatelessWidget {
   const AdminProductManagement({super.key});
