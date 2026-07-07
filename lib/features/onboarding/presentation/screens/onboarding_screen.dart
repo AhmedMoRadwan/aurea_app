7552979684098fa150fb89511d2c14a5baab9aa5
Mahmoud Hamam <mahmoudhamam093@gmail.com>
2026-07-08 00:24:44 +0300

    refactor

--- lib/features/onboarding/presentation/screens/onboarding_screen.dart
+++ lib/features/onboarding/presentation/screens/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nti_ecommerce_team4/features/auth/presentation/screens/login_screen.dart';
 
 import '../widgets/onboarding_page.dart';
 
@@ -39,37 +40,64 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             title: 'Discover  Luxury Accessories',
             description:
                 'Curated collections of the world\'s finest jewelry and accessories.',
-            imageUrl: 'assets/images/dd776c2dee1ba8531d330bb78377822cdce767c6.png',
+            imageUrl:
+                'assets/images/dd776c2dee1ba8531d330bb78377822cdce767c6.png',
             onNext: () {
               _pageController.nextPage(
                 duration: const Duration(milliseconds: 300),
                 curve: Curves.easeInOut,
               );
             },
-            onSkip: () {},
+            onSkip: () {
+              _pageController.nextPage(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+              );
+            },
           ),
           OnboardingPage(
             pageNumber: 2,
             title: 'Discover  Luxury Accessories',
             description:
                 'Curated collections of the world\'s finest jewelry and accessories.',
-            imageUrl: 'assets/images/dd776c2dee1ba8531d330bb78377822cdce767c6.png',
+            imageUrl:
+                'assets/images/dd776c2dee1ba8531d330bb78377822cdce767c6.png',
             onNext: () {
               _pageController.nextPage(
                 duration: const Duration(milliseconds: 300),
                 curve: Curves.easeInOut,
               );
             },
-            onSkip: () {},
+            onSkip: () {
+              _pageController.nextPage(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+              );
+            },
           ),
           OnboardingPage(
             pageNumber: 3,
             title: 'Discover Luxury Accessories',
             description:
                 'Curated collections of the world\'s finest jewelry and accessories.',
-            imageUrl: 'assets/images/dd776c2dee1ba8531d330bb78377822cdce767c6.png',
-            onNext: () {},
-            onSkip: () {},
+            imageUrl:
+                'assets/images/dd776c2dee1ba8531d330bb78377822cdce767c6.png',
+            onNext: () {
+              // //! navigate to login
+
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginScreen()),
+              );
+            },
+            onSkip: () {
+              // //! navigate to login
+
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginScreen()),
+              );
+            },
           ),
         ],
       ),
