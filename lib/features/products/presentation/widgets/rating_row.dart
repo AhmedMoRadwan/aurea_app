838393ee27cc5eea45104aa5f33b0dc2444aa0c0
Momen Mohamed <momenmohamed112006@gmail.com>
2026-07-07 23:41:21 +0300

    seperation classes

--- lib/features/products/presentation/widgets/rating_row.dart
+++ lib/features/products/presentation/widgets/rating_row.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:nti_ecommerce_team4/features/products/presentation/widgets/start_rating.dart';
 
 import '../screens/product_details_screen.dart';
 
 class RatingRow extends StatelessWidget {
-  const RatingRow();
+  const RatingRow({super.key});
 
   @override
   Widget build(BuildContext context) {
